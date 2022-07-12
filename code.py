41d6fa7e00032f1cb20063ca4628da85f8ff5160
Thareeq <emuajiz@gmail.com>
2022-07-12 16:12:02 +0700

    refactor

--- code.py
+++ code.py
@@ -4,55 +4,88 @@ from PIL import Image
 import numpy as np
 
 
+def psnr(A, B):
+    mse = np.mean((A - B) ** 2)
+    if(mse == 0):
+        return 100
+    max_pixel = 255.0
+    psnr = 20 * math.log10(max_pixel / math.sqrt(mse))
+    return psnr
+
+
 def readImage(filename):
     image = Image.open(filename)
     if image.mode != "L":
-        image.convert("L")
+        image = image.convert("L")
     return np.array(image)
 
 
 def splitImage(imageData, outsideWidth=32):
     imageSize = imageData.shape
     # ensure image size is square
-    # assert imageSize[0] == imageSize[1]
+    assert imageSize[0] == imageSize[1]
     insidePart = imageData[outsideWidth:imageSize[1] -
                            outsideWidth, outsideWidth:imageSize[0] - outsideWidth]
     # print(insidePart.shape)
-    outsidePart = []
-    Image.fromarray(insidePart).save("inside.png")
-    for i in range(0, imageSize[0], outsideWidth):
+
+    leftside = []
+    upside = []
+    rightside = []
+    bottomside = []
+    # Image.fromarray(insidePart).save("inside.png")
+    for i in range(0, imageSize[1], outsideWidth):
         # upside
         imageTemp = np.array(imageData[0:outsideWidth, i:i+outsideWidth])
         # print(imageTemp.shape)
-        Image.fromarray(imageTemp).save(
-            f"upside/outside-(({i},0),({i+outsideWidth},{outsideWidth})).png")
-        outsidePart.append(imageTemp)
-
-        # rightside
-        imageTemp = np.array(
-            imageData[i:i+outsideWidth, imageSize[1] - outsideWidth:imageSize[1]])
-        # print(imageTemp.shape)
-        Image.fromarray(imageTemp).save(
-            f"rightside/outside-(({imageSize[1]-outsideWidth},{i}),({imageSize[1]},{i+outsideWidth})).png")
-        outsidePart.append(imageTemp)
+        # Image.fromarray(imageTemp).save(
+        #     f"upside/outside-(({i},0),({i+outsideWidth},{outsideWidth})).png")
+        # Image.fromarray(imageTemp).save(f"upside/{i}.png")
+        upside.append(imageTemp)
 
         # bottomside
         imageTemp = np.array(
             imageData[imageSize[0] - outsideWidth:imageSize[0], imageSize[1] - i - outsideWidth:imageSize[1] - i])
         # print(imageTemp.shape)
-        Image.fromarray(imageTemp).save(
-            f"bottomside/outside-(({imageSize[1] - i - outsideWidth},{imageSize[0] - outsideWidth}),({imageSize[1] - i},{imageSize[0]})).png")
-        outsidePart.append(imageTemp)
+        # Image.fromarray(imageTemp).save(
+        #     f"bottomside/outside-(({imageSize[1] - i - outsideWidth},{imageSize[0] - outsideWidth}),({imageSize[1] - i},{imageSize[0]})).png")
+        # Image.fromarray(imageTemp).save(f"bottomside/{i}.png")
+        bottomside.append(imageTemp)
+
+    for i in range(0, imageSize[0], outsideWidth):
+        # rightside
+        imageTemp = np.array(
+            imageData[i:i+outsideWidth, imageSize[1] - outsideWidth:imageSize[1]])
+        # print(imageTemp.shape)
+        # Image.fromarray(imageTemp).save(
+        #     f"rightside/outside-(({imageSize[1]-outsideWidth},{i}),({imageSize[1]},{i+outsideWidth})).png")
+        # Image.fromarray(imageTemp).save(f"rightside/{i}.png")
+        rightside.append(imageTemp)
 
         # leftside
         imageTemp = np.array(
             imageData[imageSize[0] - i - outsideWidth:imageSize[0] - i, 0:outsideWidth])
         # print(imageTemp.shape)
-        Image.fromarray(imageTemp).save(
-            f"leftside/outside-(({0},{imageSize[0] - i - outsideWidth}),({outsideWidth},{imageSize[0] - i})).png")
-        outsidePart.append(imageTemp)
+        # Image.fromarray(imageTemp).save(
+        #     f"leftside/outside-(({0},{imageSize[0] - i - outsideWidth}),({outsideWidth},{imageSize[0] - i})).png")
+        # Image.fromarray(imageTemp).save(f"leftside/{i}.png")
+        leftside.append(imageTemp)
 
-    return insidePart, outsidePart
+    # overlapping for check
+    assert psnr(upside[0], leftside[-1])
+    assert psnr(rightside[0], upside[-1])
+    assert psnr(bottomside[0], rightside[-1])
+    assert psnr(leftside[0], bottomside[-1])
+
+    return insidePart, np.array([leftside, upside, rightside, bottomside])
+
+
+def mergeImage(insidePart, outsidePart):
+    imageSize = (insidePart.shape[0] + 2*outsidePart.shape[2],
+                 insidePart.shape[1] + 2*outsidePart.shape[3])
+    print(imageSize)
+    mergedImage = Image.fromarray(insidePart)
+    mergedImage.save("merged.png")
+    return mergedImage
 
 
 def imageHash(imageData):
@@ -63,15 +96,6 @@ def imageHash(imageData):
     return h.hexdigest()
 
 
-def psnr(A, B):
-    mse = np.mean((A - B) ** 2)
-    if(mse == 0):
-        return 100
-    max_pixel = 255.0
-    psnr = 20 * math.log10(max_pixel / math.sqrt(mse))
-    return psnr
-
-
 # print(psnr(imageTestA, imageTestB))
 imageData = readImage("original.png")
 # print(imageData[0][0] // 2)
@@ -80,19 +104,6 @@ imageData = readImage("original.png")
 # print(imageHashDigest)
 
 imageDataInside, imageDataOutside = splitImage(imageData, 32)
+# print(imageDataOutside.shape)
 
-imageTestA = readImage("upside/outside-((0,0),(32,32)).png")
-imageTestB = readImage("leftside/outside-((0,0),(32,32)).png")
-assert psnr(imageTestA, imageTestB) == 100
-
-imageTestA = readImage("upside/outside-((480,0),(512,32)).png")
-imageTestB = readImage("rightside/outside-((480,0),(512,32)).png")
-assert psnr(imageTestA, imageTestB) == 100
-
-imageTestA = readImage("bottomside/outside-((480,480),(512,512)).png")
-imageTestB = readImage("rightside/outside-((480,480),(512,512)).png")
-assert psnr(imageTestA, imageTestB) == 100
-
-imageTestA = readImage("bottomside/outside-((0,480),(32,512)).png")
-imageTestB = readImage("leftside/outside-((0,480),(32,512)).png")
-assert psnr(imageTestA, imageTestB) == 100
+mergedImageData = mergeImage(imageDataInside, imageDataOutside)
